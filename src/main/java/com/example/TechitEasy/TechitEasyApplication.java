ca04a332fe58422f8bff9672a31df9181fff4ba2
Xbendit <vanrossumben@gmail.com>
2025-05-25 12:29:54 +0200

    first commit

---
+++ src/main/java/com/example/TechitEasy/TechitEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechitEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechitEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechitEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TechitEasy/TechitEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechitEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechitEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
