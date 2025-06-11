8fc85f23bc80e5e22f23bbee0835a46afedcc2bf
Xbendit <vanrossumben@gmail.com>
2025-06-11 12:10:41 +0200

    commit 3: voor model huiswerk

--- src/main/java/com/example/TechitEasy/controllers/TelevisionsController.java
+++ src/main/java/com/example/TechitEasy/controllers/TelevisionsController.java
@@ -1,36 +1,46 @@
 package com.example.TechitEasy.controllers;
 
 import com.example.TechitEasy.exceptions.RecordNotFoundException;
-import com.example.TechitEasy.models.Televisions;
-import org.springframework.http.HttpStatus;
+import com.example.TechitEasy.models.Television;
+import com.example.TechitEasy.repository.TelevisionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/televisions")
 public class TelevisionsController {
 
-    Map<Integer, Televisions> televisionsMap = new HashMap<>();
+    private final TelevisionRepository repos;
+
+    public TelevisionsController(TelevisionRepository repos){
+    this.repos = repos;
+}
 
     @PostMapping
-    public ResponseEntity<Televisions> createTeleversions(@RequestBody Televisions televisions){
-        this.televisionsMap.put(televisions.getId(), televisions);
+    public ResponseEntity<Television> createTeleversions(@RequestBody Television televisions){
+        this.repos.save(televisions);
 
-        return new ResponseEntity<>(televisions, HttpStatus.CREATED);
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + televisions.getId()).toUriString());
+
+        return ResponseEntity.created(uri).body(televisions);
     }
 
     @GetMapping
-    public ResponseEntity<Map<Integer,Televisions>> getAllTelevisions(){
-        return ResponseEntity.ok(this.televisionsMap);
+    public ResponseEntity<List<Television>> getAllTelevisions(){
+        return ResponseEntity.ok(this.repos.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Televisions> getTelevisionById(@PathVariable int id){
-        if (this.televisionsMap.containsKey(id)){
-            return ResponseEntity.ok(televisionsMap.get(id));
+    public ResponseEntity<Television> getTelevisionById(@PathVariable int id){
+        Optional<Television> op = this.repos.findById(id);
+
+        if (op.isPresent()){
+            return ResponseEntity.ok(op.get());
         }
         else {
             //return ResponseEntity.notFound().build();
@@ -39,11 +49,13 @@ public class TelevisionsController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Televisions> updateTelevision(@PathVariable int id, @RequestBody Televisions televisions){
-        if (this.televisionsMap.containsKey(id)){
+    public ResponseEntity<Television> updateTelevision(@PathVariable int id, @RequestBody Television televisions){
+        Optional<Television> op = this.repos.findById(id);
 
-            this.televisionsMap.put(id, televisions);
-            return ResponseEntity.ok(televisions);
+        if (op.isPresent()){
+            televisions.setId(id);
+            this.repos.save(televisions);
+            return ResponseEntity.noContent().build();
         }
         else {
             return ResponseEntity.notFound().build();
@@ -52,9 +64,10 @@ public class TelevisionsController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTelevisionById(@PathVariable int id){
+        Optional<Television> op = this.repos.findById(id);
 
-        if (this.televisionsMap.containsKey(id)){
-            this.televisionsMap.remove(id);
+        if (op.isPresent()){
+            this.repos.deleteById(id);
 
             return ResponseEntity.ok("Television with ID " + id + " deleted successfully.");
         }
